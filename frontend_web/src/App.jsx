6eb086275ea846b7ef4475e69559a9ec1b60d208
Neeschal1 <neeschalpok04@gmail.com>
2026-02-10 09:53:10 +0545

    tailwind setup completed!

---
+++ frontend_web/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+const App = () => {
+  return (
+    <div className='flex w-full h-screen justify-center items-center bg-amber-300 flex-1'>
+      <h1 className='text-3xl text-blue-600 font-bold'>Neeschal Pokharel</h1>
+    </div>
+  )
+}
+
+export default App
