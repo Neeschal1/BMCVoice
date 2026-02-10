8159d52c10abac338890e0c4a0595ba2b0a6c2e2
Neeschal1 <neeschalpok04@gmail.com>
2026-02-10 09:59:38 +0545

    dom setup completed

--- frontend_web/src/App.jsx
+++ frontend_web/src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Welcome from "./pages/welcome";
+import Identity from "./pages/identity";
+import Details from "./pages/details";
+import Recheck from "./pages/recheck";
+import Success from "./pages/success";
 
 const App = () => {
   return (
-    <div className='flex w-full h-screen justify-center items-center bg-amber-300 flex-1'>
-      <h1 className='text-3xl text-blue-600 font-bold'>Neeschal Pokharel</h1>
-    </div>
-  )
-}
+    <Router>
+      <div className="flex w-full h-screen justify-center items-center flex-1">
+        <Routes>
+          <Route path="/" element={<Welcome />}/>
+          <Route path="/person" element={<Identity />}/>
+          <Route path="/person/details/" element={<Details />}/>
+          <Route path="/person/details/recheck/" element={<Recheck />}/>
+          <Route path="/person/details/recheck/success/" element={<Success />}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
