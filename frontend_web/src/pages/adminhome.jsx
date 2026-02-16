41c08b27100f5dcd3e9e539d2144678ce18f1ede
Neeschal1 <neeschalpok04@gmail.com>
2026-02-16 21:58:11 +0545

    admin home basic setup, currently in workable state

--- frontend_web/src/pages/adminhome.jsx
+++ frontend_web/src/pages/adminhome.jsx
@@ -3,6 +3,7 @@ import HandleReadData from "../api/read";
 
 const Adminhome = () => {
   const [data, setData] = useState([]);
+  const [read, setRead] = useState({})
 
   const fetchData = async () => {
     try {
@@ -15,19 +16,46 @@ const Adminhome = () => {
     }
   };
 
+  const handlePress = (index, item) => {
+console.log("Index: ", index ," and item: ", item)
+  }
+
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-slate-100">
-      {data.map((item) => (
-        <div key={item.id}>
-          <h1 className="text-black">{item.Name}</h1>
-          <h1 className="text-black">{item.Address}</h1>
-          <h1 className="text-black">{item.Phone_Number}</h1>
-          <p className="text-black">{item.Content}</p>
-          <h1 className="text-black">{item.Name}</h1>
+    <div className="flex min-h-screen w-full items-center justify-center flex-col bg-slate-100 lg:w-full sm:w-full pt-20">
+      {data.map((item, index) => (
+        <div key={index} className="w-full max-w-4xl px-4 mt-5">
+          <button
+          onClick={() => handlePress(index, item)}
+            className="
+        w-full 
+        bg-[#D8D4FF]
+        hover:bg-[#978bff]
+        transition 
+        rounded-xl 
+        p-4 
+        shadow-md
+        lg:w-full
+      "
+          >
+            <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2 text-black text-left">
+              <div className="flex flex-row items-center justify-between">
+                <p className="font-semibold">{item.Name}</p>
+                <p className="font-normal text-sm">{item.Phone_Number}</p>
+              </div>
+
+              <div className="sm:text-right -mt-2">
+                <p className="text-sm font-normal">{item.Address}</p>
+              </div>
+
+              <div className="sm:text-right">
+                <p className="text-sm font-normal">{item.Content}</p>
+              </div>
+            </div>
+          </button>
         </div>
       ))}
     </div>
